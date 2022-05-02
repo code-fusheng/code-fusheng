d1e86fcec538b74638669274736a6bcb59f00d63
code-fusheng <2561035977@qq.com>
2022-05-02 14:09:10 +0800

    feature : 错误案例 --- 新增 ConcurrentHashMap 使用案例以及说明

--- doc/wiki/README.md
+++ doc/wiki/README.md
@@ -14,6 +14,8 @@ feature : 认证授权 --- 新增用户名密码登录login接口以及用户信
 feature : 线程变量 --- 新增本地线程变量 ThreadLocalContext 持有者类
 feature : 切面模版 --- 新增切面配置参考模版
 feature : 错误案例 --- 新增错误案例以及正常处理 Demo 分支 master-error
+feature : 错误案例 --- 新增 ThreadLocal 使用案例以及说明
+feature : 错误案例 --- 新增 ConcurrentHashMap 使用案例以及说明
 ```
 
 
--- src/main/java/xyz/fusheng/project/controller/com/ComTestController.java
+++ src/main/java/xyz/fusheng/project/controller/com/ComTestController.java
@@ -9,6 +9,17 @@ import org.springframework.web.bind.annotation.*;
 import xyz.fusheng.project.common.holder.ThreadLocalContext;
 import xyz.fusheng.project.model.base.BaseResult;
 
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
 /**
  * @FileName: ComTestController
  * @Author: code-fusheng
@@ -46,9 +57,18 @@ public class ComTestController {
      * 但是！因为 Spring Boot 的多线程依赖 web 容器 tomcat 的线程池机制，而线程池有线程复用的机制，
      * 当并发请求过大时，会导致 ThreadLocal 数据混乱，所以需要显示的做 ThreadLocal 数据清除。
      */
-    @ApiOperation("测试ThreadLocal并发问题")
-    @GetMapping("/testThreadLocal")
-    public BaseResult<Object> testThreadLocal(@RequestParam("id") Integer id) {
+    @ApiOperation("测试ThreadLocal并发问题-错误")
+    @GetMapping("/testThreadLocalError")
+    public BaseResult<Object> testThreadLocalError(@RequestParam("id") Integer id) {
+        logger.info("[ThreadLocal-Before] => mark:{}", ThreadLocalContext.get().getMark());
+        ThreadLocalContext.get().setMark("false");
+        logger.info("[ThreadLocal-After] => mark:{}", ThreadLocalContext.get().getMark());
+        return BaseResult.success(ThreadLocalContext.get().getMark());
+    }
+
+    @ApiOperation("测试ThreadLocal并发问题-正确")
+    @GetMapping("/testThreadLocalTrue")
+    public BaseResult<Object> testThreadLocalTrue(@RequestParam("id") Integer id) {
         try {
             logger.info("[ThreadLocal-Before] => mark:{}", ThreadLocalContext.get().getMark());
             ThreadLocalContext.get().setMark("false");
@@ -58,5 +78,87 @@ public class ComTestController {
             ThreadLocalContext.get().removeAll();
         }
     }
+
+    // 辅助方法 - 用来获取一个指定元素量的模拟 ConcurrentHashMap 数据
+    private ConcurrentHashMap<String, Long> getData(int count) {
+        return LongStream.rangeClosed(1, count)
+                .boxed()
+                .collect(Collectors.toConcurrentMap(i -> UUID.randomUUID().toString(),
+                        Function.identity(),
+                        (o1, o2) -> o1, ConcurrentHashMap::new));
+    }
+
+    /**
+     * TODO ConcurrentHashMap 聚合方法非线程安全问题
+     * ConcurrentHashMap 虽然提供了数据结构上的线程安全技术支持，但是在 诸如 size、isEmpty、containsValue 等聚合方法上
+     * 在并发情况下，可能会反映其中间状态或信息
+     * 解决方案: size 部分逻辑加锁
+     * PS: 这样无法充分发挥工具的特性，ConcurrentHashMap 提供了一些原子性的复合逻辑方法，例如: ComputeIfAbsent
+     * ComputeIfAbsent 底层通过 Java 自带的 Unsafe 实现 CAS，在虚拟机层面确保了写入数据的原子性。
+     */
+
+    @ApiOperation("测试ConcurrentHashMap并发问题-错误")
+    @GetMapping("/testConcurrentHashMapError")
+    public BaseResult<Object> testConcurrentHashMapError() throws InterruptedException {
+        // 初始化数据 size = 900
+        ConcurrentHashMap<String, Long> concurrentHashMap = getData(1000 - 100);
+        logger.info("[ConcurrentHashMap] => initSize:{}", concurrentHashMap.size());
+        // 使用线程池并发处理
+        ForkJoinPool forkJoinPool = new ForkJoinPool(10);
+        forkJoinPool.execute(() -> IntStream.rangeClosed(1, 10).parallel().forEach(i -> {
+            // 查询还要补充多少元素到 1000
+            int needSize = 1000 - concurrentHashMap.size();
+            logger.info("[ConcurrentHashMap] => needSize:{}", needSize);
+            // 根据需要的量补充元素
+            concurrentHashMap.putAll(getData(needSize));
+        }));
+        // 等待所有任务完成
+        forkJoinPool.shutdown();
+        forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+        // 最后的元素大小
+        logger.info("[ConcurrentHashMap] => finalSize:{}", concurrentHashMap.size());
+        return BaseResult.success(concurrentHashMap);
+    }
+
+    @ApiOperation("测试ConcurrentHashMap并发问题-正确")
+    @GetMapping("/testConcurrentHashMapTrue")
+    public BaseResult<Object> testConcurrentHashMapTrue() throws InterruptedException {
+        // 初始化数据 size = 900
+        ConcurrentHashMap<String, Long> concurrentHashMap = getData(1000 - 100);
+        logger.info("[ConcurrentHashMap] => initSize:{}", concurrentHashMap.size());
+        // 使用线程池并发处理
+        ForkJoinPool forkJoinPool = new ForkJoinPool(10);
+        forkJoinPool.execute(() -> IntStream.rangeClosed(1, 10).parallel().forEach(i -> {
+            synchronized (concurrentHashMap) {
+                // 查询还要补充多少元素到 1000
+                int needSize = 1000 - concurrentHashMap.size();
+                logger.info("[ConcurrentHashMap] => needSize:{}", needSize);
+                // 根据需要的量补充元素
+                concurrentHashMap.putAll(getData(needSize));
+            }
+        }));
+        // 等待所有任务完成
+        forkJoinPool.shutdown();
+        forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+        // 最后的元素大小
+        logger.info("[ConcurrentHashMap] => finalSize:{}", concurrentHashMap.size());
+        return BaseResult.success(concurrentHashMap);
+    }
+
+    @ApiOperation("测试ConcurrentHashMap并发问题-优化")
+    @GetMapping("/testConcurrentHashMapPro")
+    public BaseResult<Object> testConcurrentHashMapPro() throws InterruptedException {
+        ConcurrentHashMap<String, LongAdder> maps = new ConcurrentHashMap<>(10);
+        ForkJoinPool forkJoinPool = new ForkJoinPool(10);
+        forkJoinPool.execute(() -> IntStream.rangeClosed(1, 10000000).parallel().forEach(i -> {
+            String key = "item" + ThreadLocalRandom.current().nextInt(10);
+            // 利用 computeIfAbsent 方法实力话 LongAdder，利用此进行线程安全记数
+            maps.computeIfAbsent(key, k -> new LongAdder()).increment();
+        }));
+        forkJoinPool.shutdown();
+        forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+        return BaseResult.success(maps);
+    }
+
 
 }
