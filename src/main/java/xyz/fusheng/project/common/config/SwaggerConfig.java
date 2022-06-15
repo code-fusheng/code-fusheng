bdf4938199010741d70ef24ff5f087ab84691662
code-fusheng <2561035977@qq.com>
2022-06-15 11:40:31 +0800

    fix : 架构设计 --- 新增 swagger 接口文档配置

---
+++ src/main/java/xyz/fusheng/project/common/config/SwaggerConfig.java
@@ -0,0 +1,88 @@
+package xyz.fusheng.project.common.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @FileName: SwaggerConfig
+ * @Author: code-fusheng
+ * @Date: 2020/4/29 10:21
+ * Description: Swagger配置类
+ */
+
+@Configuration
+@EnableSwagger2
+@EnableKnife4j
+public class SwaggerConfig {
+
+    /**
+     * 配置一个 Docket Bean
+     * 配置映射路径与需要扫描的接口位置
+     *
+     * @return
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .securityContexts(Arrays.asList())
+                ;
+    }
+
+    /**
+     * 配置全局参数
+     *
+     * @return
+     */
+    private SecurityScheme securityScheme() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    /**
+     * 配置了那些请求需要携带 Token
+     *
+     * @return
+     */
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("xxx", "描述信息");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    /**
+     * 主要配置接口网站的信息
+     * http://localhost:9999/doc.html
+     *
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("个人论坛接口API")
+                .contact(new Contact("code-fusheng", "https:fusheng.xyz", "2561035977@qq.com"))
+                .description("Api").version("1.0.1")
+                .termsOfServiceUrl("sv").build();
+    }
+}
